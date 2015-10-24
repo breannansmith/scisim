5d8d329a4253a22f9ce4014b22434f6ab5fa0999
Breannan Smith <smith@cs.columbia.edu>
2015-10-23 19:21:49 -0700

    Reverted the Staggered Projections implementation to one that more closely matches the original paper to avoid obscure corner cases.

--- scisim/ConstrainedMaps/StaggeredProjections.cpp
+++ scisim/ConstrainedMaps/StaggeredProjections.cpp
@@ -1,7 +1,7 @@
 // StaggeredProjections.cpp
 //
 // Breannan Smith
-// Last updated: 09/07/2015
+// Last updated: 10/23/2015
 
 #include "StaggeredProjections.h"
 
@@ -36,90 +36,6 @@ StaggeredProjections::StaggeredProjections( std::istream& input_stream )
 StaggeredProjections::~StaggeredProjections()
 {}
 
-//static scalar computeGlobalObjectiveSingleCoR( const SparseMatrixsc& Minv, const VectorXs& v0, const scalar& CoR, const VectorXs& mu, const SparseMatrixsc& N, const VectorXs& alpha, const SparseMatrixsc& D, const VectorXs& beta, const VectorXs& lambda, const VectorXs& nrel, const VectorXs& drel )
-//{
-//  // || N \alpha + D \beta ||^2_{M^{-1}}
-//  assert( N.cols() == alpha.size() ); assert( D.cols() == beta.size() );
-//  assert( N.rows() == D.rows() ); assert( Minv.cols() == N.rows() );
-//  assert( Minv.cols() == Minv.rows() );
-//  const scalar termA = ( N * alpha + D * beta ).dot( Minv * ( N * alpha + D * beta ) );
-//
-//  // ( (1+e) * N \alpha + D \beta )^T \dot{q}^p
-//  assert( v0.size() == N.rows() ); assert( CoR >= 0.0 ); assert( CoR <= 1.0 );
-//  const scalar termB = v0.dot( ( 1 + CoR ) * N * alpha + D * beta );
-//
-//  // ( 1 + e ) \alpha^T \dot{g}_N + \beta^T drel
-//  assert( alpha.size() == nrel.size() ); assert( beta.size() == drel.size() );
-//  const scalar termC = ( 1 + CoR ) * alpha.dot( nrel ) + beta.dot( drel );
-//
-//  // lambda^T diag( \mu ) \alpha
-//  assert( lambda.size() == mu.size() ); assert( mu.size() == alpha.size() );
-//  const scalar termD =  lambda.dot( ( mu.array() * alpha.array() ).matrix() );
-//
-//  return termA + termB + termC + termD;
-//}
-
-//static scalar computeGlobalError( const SparseMatrixsc& Minv, const VectorXs& v0, const VectorXs& CoR, const VectorXs& mu, const SparseMatrixsc& N, const VectorXs& alpha, const SparseMatrixsc& D, const VectorXs& beta, const VectorXs& lambda, const VectorXs& nrel, const VectorXs& drel )
-//{
-//  // || N \alpha + D \beta ||^2_{M^{-1}}
-//  assert( N.cols() == alpha.size() ); assert( D.cols() == beta.size() );
-//  assert( N.rows() == D.rows() ); assert( Minv.cols() == N.rows() );
-//  assert( Minv.cols() == Minv.rows() );
-//  const scalar termA = ( N * alpha + D * beta ).dot( Minv * ( N * alpha + D * beta ) );
-//
-//  // (  N ( 1 + diag(e) ) \alpha )^T \dot{q}^p
-//  assert( CoR.size() == alpha.size() ); assert( ( CoR.array() >= 0.0 ).all() ); assert( ( CoR.array() <= 1.0 ).all() );
-//  const scalar termB = v0.dot( ( N * ( ( 1.0 + CoR.array() ) * alpha.array() ).matrix() ) );
-//
-//  // ( D \beta )^T \dot{q}^p
-//  assert( v0.size() == D.rows() );
-//  const scalar termC = v0.dot( D * beta );
-//
-//  // ( ( 1 + diag(e) ) \alpha )^T \dot{g}_N
-//  assert( alpha.size() == nrel.size() );
-//  const scalar termD = nrel.dot( ( ( 1.0 + CoR.array() ) * alpha.array() ).matrix() );
-//
-//  // \beta^T drel
-//  assert( beta.size() == drel.size() );
-//  const scalar termE = beta.dot( drel );
-//
-//  // lambda^T diag( \mu ) \alpha
-//  assert( lambda.size() == mu.size() ); assert( mu.size() == alpha.size() );
-//  const scalar termF = lambda.dot( ( mu.array() * alpha.array() ).matrix() );
-//
-//  // See if it reduces to the single CoR version
-//  #ifndef NDEBUG
-//  if( CoR.isConstant( CoR(0), 0.0 ) )
-//  {
-//    const scalar single_cor_obj = computeGlobalObjectiveSingleCoR( Minv, v0, CoR(0), mu, N, alpha, D, beta, lambda, nrel, drel );
-//    assert( fabs( single_cor_obj - ( termA + termB + termC + termD + termE + termF ) ) <= 1.0e-6 );
-//  }
-//  #endif
-//
-//  return termA + termB + termC + termD + termE + termF;
-//}
-
-//static void computeLambda( const std::vector<std::unique_ptr<Constraint>>& active_set, const VectorXs& q0, const VectorXs& v0, const SparseMatrixsc& Minv, const SparseMatrixsc& N, const VectorXs& alpha, const SparseMatrixsc& D, const VectorXs& beta, VectorXs& lambda )
-//{
-//  lambda.resize( active_set.size() );
-//  assert( Minv.rows() == Minv.cols() ); assert( Minv.rows() == v0.size() );
-//  assert( Minv.cols() == N.rows() ); assert( Minv.cols() == D.rows() );
-//  assert( N.cols() == alpha.size() ); assert( D.cols() == beta.size() );
-//  const VectorXs v_temp = v0 + Minv * ( N * alpha + D * beta );
-//  for( unsigned i = 0; i < lambda.size(); ++i )
-//  {
-//    lambda( i ) = active_set[i]->computeLambda( q0, v_temp );
-//  }
-//}
-
-//static scalar globalObjective( const std::vector<std::unique_ptr<Constraint>>& active_set, const SparseMatrixsc& Minv, const VectorXs& q0, const VectorXs& v0, const VectorXs& CoR, const VectorXs& mu, const SparseMatrixsc& N, const VectorXs& alpha, const SparseMatrixsc& D, const VectorXs& beta, const VectorXs& nrel, const VectorXs& drel )
-//{
-//  VectorXs lambda;
-//  computeLambda( active_set, q0, v0, Minv, N, alpha, D, beta, lambda );
-//  return computeGlobalError( Minv, v0, CoR, mu, N, alpha, D, beta, lambda, nrel, drel );
-//}
-
-
 // Verify that || M^-1 N \alpha ||_M^2  <= || (1 + cor) v0 + M^-1 f0 ||_M^2
 // TODO: Update for kinematic boundaries
 // TODO: Pass tolerance in as option
@@ -129,7 +45,7 @@ static bool impactSolutionIsContraction( const SparseMatrixsc& M, const SparseMa
 {
   if( ( nrel.array() != 0.0 ).any() ) { return true; }
   if( ( drel.array() != 0.0 ).any() ) { return true; }
-    if( ( CoR.array() != CoR(0) ).any() ) { return true; }
+  if( ( CoR.array() != CoR(0) ).any() ) { return true; }
 
   // || M^-1 N \alpha ||_M^2
   const scalar first{ ( Minv * N * alpha ).dot( N * alpha ) };
@@ -218,17 +134,24 @@ static scalar computeSobogusError( const VectorXs& alpha, const VectorXs& beta, 
 {
   VectorXs per_contact_force;
   computePerContactForce( alpha, beta, contact_bases, per_contact_force );
-  const scalar error{ sobogus_problem.computeError( per_contact_force ) };
-  return error;
+  return sobogus_problem.computeError( per_contact_force );
 }
 
+// TODO: Pre-allocate space for temporaries
+// TODO: Make as many variables const as possible (D, N, nrel, drel, ...)
+// TODO: Unify interfces for formGeneralizedSmoothFrictionBasis and computeN
+// TODO: Use the improved matrix-vector routines
 // NOTE: Can't precompute linear terms as they change during the solve
 void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, const FlowableSystem& fsys, const SparseMatrixsc& M, const SparseMatrixsc& Minv, const VectorXs& CoR, const VectorXs& mu, const VectorXs& q0, const VectorXs& v0, std::vector<std::unique_ptr<Constraint>>& active_set, const MatrixXXsc& contact_bases, const unsigned max_iters, const scalar& tol, VectorXs& f, VectorXs& alpha, VectorXs& beta, VectorXs& vout, bool& solve_succeeded, scalar& error )
 {
-  assert( MathUtilities::isSquare( M ) ); assert( MathUtilities::isSquare( Minv ) ); assert( M.rows() == Minv.rows() );
-  assert( CoR.size() == alpha.size() ); assert( CoR.size() == mu.size() );
+  assert( MathUtilities::isSquare( M ) );
+  assert( MathUtilities::isSquare( Minv ) );
+  assert( M.rows() == Minv.rows() );
+  assert( CoR.size() == alpha.size() );
+  assert( CoR.size() == mu.size() );
   assert( active_set.size() == std::vector<std::unique_ptr<Constraint>>::size_type( mu.size() ) );
-  assert( v0.size() == M.cols() ); assert( vout.size() == v0.size() );
+  assert( v0.size() == M.cols() );
+  assert( vout.size() == v0.size() );
   assert( contact_bases.cols() % contact_bases.rows() == 0 );
   assert( active_set.size() == unsigned( contact_bases.cols() / contact_bases.rows() ) );
   assert( beta.size() == contact_bases.cols() - alpha.size() );
@@ -246,23 +169,13 @@ void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, co
   Constraint::evalKinematicRelVelGivenBases( q0, v0, active_set, contact_bases, nrel, drel );
 
   // For computing the error
-  // TODO: Make a constructor for this that takes the sparse matrix directly!
+  // TODO: Move this to a member variable that is passed into the solver, to make it easy to change the implementation later
   assert( fsys.numVelDoFsPerBody() == 2 || fsys.numVelDoFsPerBody() == 3 || fsys.numVelDoFsPerBody() == 6 );
   SobogusFrictionProblem sbfp( fsys.numVelDoFsPerBody() == 2 ? SobogusSolverType::Balls2D : fsys.numVelDoFsPerBody() == 3 ? SobogusSolverType::RigidBody2D : SobogusSolverType::RigidBodies3D );
   {
     VectorXs flat_masses;
     sbfp.flattenMass( M, flat_masses );
     sbfp.initialize( active_set, contact_bases, flat_masses, q0, v0, CoR, mu, nrel, drel );
-  }
-  scalar global_error{ computeSobogusError( alpha, beta, contact_bases, sbfp ) };
-  error = global_error;
-  // Absolute tolerance
-  if( global_error <= tol )
-  {
-    f = D * beta;
-    vout = v0 + Minv * ( N * alpha + D * beta );
-    solve_succeeded = true;
-    return;
   }
 
   // Quadratic term in LCP QP
@@ -275,9 +188,13 @@ void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, co
   VectorXs best_alpha{ alpha };
   VectorXs best_beta{ beta };
   unsigned best_iteration{ 0 };
+  // Note: Force the solver to use a subsequent iterate by setting the initial error to infinity.
+  //       Occasionally, an initial guess of all zeros will be selected as the best solution if
+  //       Staggered Projections fails to terminate.
+  error = SCALAR_INFINITY;
+  solve_succeeded = false;
 
   // Staggered projections loop to compute coupled impact/friction
-  solve_succeeded = false;
   for( unsigned itr = 0; itr < max_iters; ++itr )
   {
     // Impact solve
@@ -294,33 +211,6 @@ void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, co
       assert( impactSolutionIsContraction( M, Minv, N, CoR, v0, vbeta, alpha, nrel, drel ) );
     }
 
-    // Compute the new global error
-    global_error = computeSobogusError( alpha, beta, contact_bases, sbfp );
-    //fabs( globalObjective( active_set, Minv, q0, v0, CoR, mu, N, alpha, D, beta, nrel, drel ) );
-
-    // If the current solution is the best yet, cache it
-    if( global_error < error )
-    {
-      error = global_error;
-      best_beta = beta;
-      best_alpha = alpha;
-      best_iteration = itr;
-    }
-
-    // TODO: If we bail here on the first iteration, D \beta might not equal f
-
-    // Absolute tolerance
-    if( global_error <= tol )
-    {
-      assert( global_error == error );
-      assert( ( beta.array() == best_beta.array() ).all() );
-      assert( ( alpha.array() == best_alpha.array() ).all() );
-      solve_succeeded = true;
-      f = D * beta;
-      vout = v0 + Minv * ( N * alpha + D * beta );
-      break;
-    }
-
     // Friction solve
     {
       // Incoming velocity with the impact impulses applied
@@ -333,7 +223,7 @@ void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, co
       }
       {
         // TODO: Get rid of lambda from the friction operator
-        VectorXs temp_lambda( mu.size() );
+        VectorXs temp_lambda{ mu.size() };
         m_friction_operator->flow( iteration * dt, Minv, vaplha, D, QD, drel, mu, alpha, beta, temp_lambda );
       }
       f = D * beta;
@@ -342,21 +232,73 @@ void StaggeredProjections::solve( const unsigned iteration, const scalar& dt, co
       // Verify that || vinit + M^-1 f + M^-1 N \alpha ||_M^2 <= || vinit + M^-1 N \alpha ||_M^2
       assert( frictionSolutinIsContractionB( M, Minv, f, vaplha, nrel, drel ) );
     }
+
+    // Compute the new global error
+    const scalar global_error{ computeSobogusError( alpha, beta, contact_bases, sbfp ) };
+
+    // If the current solution is the best yet, cache it
+    if( global_error < error )
+    {
+      error = global_error;
+      best_beta = beta;
+      best_alpha = alpha;
+      best_iteration = itr;
+    }
+
+    // Absolute tolerance
+    if( global_error <= tol )
+    {
+      assert( error == global_error );
+      assert( ( beta.array() == best_beta.array() ).all() );
+      assert( ( alpha.array() == best_alpha.array() ).all() );
+      assert( ( f - D * beta ).lpNorm<Eigen::Infinity>() == 0.0 );
+      solve_succeeded = true;
+      break;
+    }
   }
+
   // If the solve failed, current iteration might not be the best, so fall back on the best solution
   if( !solve_succeeded )
   {
     assert( error > tol );
-    f = D * best_beta;
+    //beta.swap( best_beta );
     beta = best_beta;
+    //alpha.swap( best_alpha );
     alpha = best_alpha;
-    vout = v0 + Minv * ( N * alpha + D * beta );
-    std::cerr << "Warning, staggered projections failed with best error " << error << ", falling back to best solution at iteration " << best_iteration << std::endl;
+    f = D * beta;
+    std::cerr << "Warning, staggered projections failed, falling back to best solution at iteration: " << best_iteration << std::endl;
   }
-  assert( ( beta.array() == best_beta.array() ).all() );
-  assert( ( f - D * beta ).lpNorm<Eigen::Infinity>() <= 1.0e-10 );
-  assert( ( alpha.array() == best_alpha.array() ).all() );
+
   assert( error == computeSobogusError( alpha, beta, contact_bases, sbfp ) );
+  assert( ( beta.array() == best_beta.array() ).all() );
+  assert( ( alpha.array() == best_alpha.array() ).all() );
+  assert( ( f - D * beta ).lpNorm<Eigen::Infinity>() == 0.0 );
+
+  // Final impact solve to prevent penetration
+  {
+    // Incoming velocity with the friction impulses applied
+    const VectorXs vbeta{ v0 + Minv * f };
+    // Solve for the impact impulses given the total friction impulse
+    if( !m_warm_start_alpha )
+    {
+      alpha.setZero();
+    }
+    m_impact_operator->flow( active_set, M, Minv, q0, v0, vbeta, N, QN, nrel, CoR, alpha );
+    // Verify that || M^-1 N \alpha ||_M^2  <= || (1 + cor) v0 + M^-1 f0 ||_M^2
+    assert( impactSolutionIsContraction( M, Minv, N, CoR, v0, vbeta, alpha, nrel, drel ) );
+  }
+
+  // Compute the final velocity
+  // TODO: assert vout == vbeta + Minv * N * alpha
+  vout = v0 + Minv * ( N * alpha + f );
+
+  // Compute the final error
+  error = computeSobogusError( alpha, beta, contact_bases, sbfp );
+  solve_succeeded = error <= tol;
+  if( !solve_succeeded )
+  {
+    std::cerr << "Warning, staggered projections failed with error: " << error << std::endl;
+  }
 }
 
 unsigned StaggeredProjections::numFrictionImpulsesPerNormal( const unsigned ambient_space_dimensions ) const
