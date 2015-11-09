3ef50753f8ac0c6f64b9f088a76814fd10c352e5
Breannan Smith <smith@cs.columbia.edu>
2015-11-08 23:47:19 -0800

    Updated Eigen to 3.2.7

--- scripts_include/get_eigen.sh
+++ scripts_include/get_eigen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-actual_eigen_tar_md5="21a928f6e0f1c7f24b6f63ff823593f5"
-eigen_url="http://bitbucket.org/eigen/eigen/get/3.2.5.tar.bz2"
-eigen_file_name="3.2.5.tar.bz2"
-extracted_eigen_name="eigen-eigen-bdd17ee3b1b3"
+actual_eigen_tar_md5="cc1bacbad97558b97da6b77c9644f184"
+eigen_url="http://bitbucket.org/eigen/eigen/get/3.2.7.tar.bz2"
+eigen_file_name="3.2.7.tar.bz2"
+extracted_eigen_name="eigen-eigen-b30b87236a1b"
 # md5 on installed Eigen source files
-actual_installed_eigen_md5="2539903ddef842de07c5a6047922ee8b"
+actual_installed_eigen_md5="466101b7d60b79f4ea34f1e14f3488ca"
 
 # Verify that wget is installed
 command -v wget >/dev/null 2>&1 || { echo >&2 "Error, please install wget and rerun get_eigen.sh."; exit 1; }
