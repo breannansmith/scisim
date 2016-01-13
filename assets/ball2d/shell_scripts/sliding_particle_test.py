653e3e37b7e1c78c24e464cb80cce0176ead9b55
Breannan Smith <smith@cs.columbia.edu>
2016-01-12 17:47:55 -0800

    Cleaned up Ball2D friction python test scripts.

--- assets/ball2d/shell_scripts/sliding_particle_test.py
+++ assets/ball2d/shell_scripts/sliding_particle_test.py
@@ -1,58 +1,62 @@
+''' Compares a simulation of a ball sliding down a plane to the analytical solution. '''
+
+import os
 import sys
-import h5py
-import numpy
 import argparse
 import math
+import h5py
 
-parser = argparse.ArgumentParser( description='Check if the position of a sliding particle on a plane is consistent with theory.' )
+parser = argparse.ArgumentParser( description='Compares a simulation of a ball sliding down a plane to the analytical solution.' )
 parser.add_argument( '-i', metavar='input_hdf5_file', type=str, nargs=1, help='input HDF5 file name', required=True )
 args = parser.parse_args()
 
 input_file_name = args.i[0]
+if not os.path.isfile(input_file_name):
+  sys.exit('Error, input file \'' + input_file_name + '\' does not exist.')
 
-print "Processing:", input_file_name
+print 'Validating:', input_file_name
 
 try:
-  h5_file = h5py.File( input_file_name, 'r' )
-  q = h5_file['q'][:]
-  v = h5_file['v'][:]
-  pln_nrmls = h5_file['static_plane_normals'][:]
-  iteration = h5_file['iteration'][0,0]
-  timestep = h5_file['timestep'][0,0]
-  h5_file.close()
-except:
-  print 'Failed to read in HDF5 file, exiting.'
-  sys.exit(1)
+  with h5py.File( input_file_name, 'r' ) as h5_file:
+    q = h5_file['q'][:]
+    v = h5_file['v'][:]
+    pln_nrmls = h5_file['static_plane_normals'][:]
+    iteration = h5_file['iteration'][0,0]
+    timestep = h5_file['timestep'][0,0]
+except IOError as io_exception:
+  sys.exit('HDF5 IO Error: ' + io_exception.message)
+except KeyError as key_exception:
+  sys.exit('HDF5 Key Error: ' + key_exception.message)
 
 time = iteration * timestep
 
+# Note: Gravity and friction are hardcoded, for now
+gravity_magnitude = 10.0
+mu = 0.5
+
 # Compute the angle of the plane
 theta = abs( math.atan2( pln_nrmls[0,0], pln_nrmls[1,0] ) )
-# Compute the analytical position of the particle
-dplane = 0.5 * time * time * ( -10.0 * math.sin(theta) + 5.0 * math.cos(theta) )
-dx =  dplane * math.cos(theta)
-if pln_nrmls[0,0] > 0 : 
+# Compute the analytical position of the ball
+dplane = 0.5 * time * time * ( -gravity_magnitude * math.sin(theta) + gravity_magnitude * mu * math.cos(theta) )
+dx = dplane * math.cos(theta)
+if pln_nrmls[0,0] > 0:
   dx *= -1.0
-dy =  dplane * math.sin(theta)
-# Compute the analytical velocity of the partcile
+dy = dplane * math.sin(theta)
+# Compute the analytical velocity of the ball
 vplane = time * ( -10.0 * math.sin(theta) + 5.0 * math.cos(theta) )
-vx =  vplane * math.cos(theta)
-if pln_nrmls[0,0] > 0 : 
+vx = vplane * math.cos(theta)
+if pln_nrmls[0,0] > 0:
   vx *= -1.0
-vy =  vplane * math.sin(theta)
+vy = vplane * math.sin(theta)
 
 # Can tighten these tolerances further by decreasing the time step, but then the simulations are slow
-if abs( q[0] - dx ) > 5.1e-5 :
-  print "First q component incorect"
-  sys.exit(1)
-if abs( q[1] - dy ) > 0.00025 :
-  print "Second q component incorect"
-  sys.exit(1)
-if abs( vx - v[0] ) > 2.03e-05 : 
-  print "First v component incorrect"
-  sys.exit(1)
-if abs( vy - v[1] ) > 9.62e-05 : 
-  print "Second v component incorrect"
-  sys.exit(1)
+if abs( q[0] - dx ) > 5.1e-5:
+  sys.exit('First q component incorect')
+if abs( q[1] - dy ) > 0.00025:
+  sys.exit('Second q component incorect')
+if abs( vx - v[0] ) > 2.03e-05:
+  sys.exit('First v component incorrect')
+if abs( vy - v[1] ) > 9.62e-05:
+  sys.exit('Second v component incorrect')
 
 sys.exit(0)
--- assets/ball2d/shell_scripts/static_particle_test.py
+++ assets/ball2d/shell_scripts/static_particle_test.py
@@ -1,37 +1,37 @@
-import sys
-import h5py
-import numpy
-import argparse
+''' Checks whether a simulation that should be static is static. '''
 
-parser = argparse.ArgumentParser( description='Determine if a simulation is static.' )
+import os
+import sys
+import argparse
+import h5py
+
+parser = argparse.ArgumentParser( description='Checks whether a simulation that should be static is static.' )
 parser.add_argument( '-i', metavar='input_hdf5_file', type=str, nargs=1, help='input HDF5 file name', required=True )
 args = parser.parse_args()
 
 input_file_name = args.i[0]
+if not os.path.isfile(input_file_name):
+  sys.exit('Error, input file \'' + input_file_name + '\' does not exist.')
 
-print "Processing:", input_file_name
+print 'Validating:', input_file_name
 
 try:
-  h5_file = h5py.File( input_file_name, 'r' )
-  q = h5_file['q'][:]
-  v = h5_file['v'][:]
-  h5_file.close()
-except:
-  print 'Failed to read in HDF5 file, exiting.'
-  sys.exit(1)
+  with h5py.File( input_file_name, 'r' ) as h5_file:
+    q = h5_file['q'][:]
+    v = h5_file['v'][:]
+except IOError as io_exception:
+  sys.exit('HDF5 IO Error: ' + io_exception.message)
+except KeyError as key_exception:
+  sys.exit('HDF5 Key Error: ' + key_exception.message)
 
 # Can tighten these tolerances further by decreasing the time step, but then the simulations are slow
-if abs( q[0] ) > 1.0e-12 :
-  print "First q component incorect."
-  sys.exit(1)
-if abs( q[1] ) > 3.0e-4 :
-  print "Second q component incorect."
-  sys.exit(1)
-if abs( v[0] ) > 1.0e-12 : 
-  print "First v component incorrect."
-  sys.exit(1)
-if abs( v[1] ) > 2.0e-4 : 
-  print "Second v component incorrect."
-  sys.exit(1)
+if abs( q[0] ) > 1.0e-12:
+  sys.exit('First q component incorect')
+if abs( q[1] ) > 3.0e-4:
+  sys.exit('Second q component incorect')
+if abs( v[0] ) > 1.0e-12:
+  sys.exit('First v component incorrect')
+if abs( v[1] ) > 2.0e-4:
+  sys.exit('Second v component incorrect')
 
 sys.exit(0)
